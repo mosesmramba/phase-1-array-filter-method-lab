8e428c295da4918582a91259d57e0aa191052f1b
mosesmramba <moses.mramba@student.moringaschool.com>
2023-09-26 11:09:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+  function findMatching(drivers, name){
+  return drivers.filter(function (driver) 
+  { return driver.toLowerCase(name) === name.toLowerCase() });
+}
+ function fuzzyMatch(drivers, letters){
+  return drivers.filter(function(driver)
+  {return driver.startsWith(letters)});
+  }
+
+  function matchName(drivers,name){
+    return drivers.filter(function(driver)
+    {return driver.name===name});
+  }
+
+ 
+ 
